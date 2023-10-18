f565861d97628ef60dc0cb8b4134c4a2d613ab20
James <66404238+JSulley@users.noreply.github.com>
2023-10-18 06:40:28 -0500

    Modified 'sections' elements to objects

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
-const socials = new Map([
-  ["social", "hyperlink"],
-]);
-
 const sections = [
-  "About Me",
-  "Projects",
-  "Certifications"
-];
+  {
+    'title': 'About Me',
+    'content': 'Est dolore mollit elit proident non elit ut aliquip duis cillum esse. Aliqua reprehenderit consequat in proident ad ut eiusmod dolor nostrud exercitation amet exercitation. Anim anim laborum aliqua ad amet laboris laborum magna.',
+  },
+  {
+    'title': 'Projects',
+    'content': 'Elit veniam magna id mollit eu aliqua ex velit laboris.'
+  },
+  {
+    'title': 'Certifications',
+    'content': "Ipsum laboris proident nulla duis velit nostrud reprehenderit tempor adipisicing irure non."
+  },
+]
 
 export default function Home() {
   return (
@@ -15,7 +20,12 @@ export default function Home() {
       <h2>Aspiring Data Scientist/Software Engineer</h2>
       <nav>
         <ul>
-          {sections.map((section) => <li>{section}</li>)}
+          {sections.map(section => (
+            <div>
+              <li>{section.title}</li>
+              <p>{section.content}</p>
+            </div>
+          ))}
         </ul>
       </nav>
     </main>
