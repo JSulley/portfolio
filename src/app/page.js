494acb75dd2ba2c9f97a07d3a51235eb20af0935
JS <66404238+JSulley@users.noreply.github.com>
2023-10-23 21:56:15 -0500

    Added content

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,33 @@
-const sections = [
-  {
-    'title': 'About Me',
-    'content': 'Est dolore mollit elit proident non elit ut aliquip duis cillum esse. Aliqua reprehenderit consequat in proident ad ut eiusmod dolor nostrud exercitation amet exercitation. Anim anim laborum aliqua ad amet laboris laborum magna.',
-  },
-  {
-    'title': 'Projects',
-    'content': 'Elit veniam magna id mollit eu aliqua ex velit laboris.'
-  },
-  {
-    'title': 'Certifications',
-    'content': "Ipsum laboris proident nulla duis velit nostrud reprehenderit tempor adipisicing irure non."
-  },
-]
-
 export default function Home() {
   return (
     <main>
       <h1>James Sullivan</h1>
       <h2>Aspiring Data Scientist/Software Engineer</h2>
-      <nav>
-        <ul>
-          {sections.map(section => (
-            <div>
-              <li>{section.title}</li>
-              <p>{section.content}</p>
-            </div>
-          ))}
-        </ul>
-      </nav>
+      <h3>About Me</h3>
+      <p>Sint Lorem est dolor labore exercitation Lorem nisi laboris ex mollit.</p>
+
+      <h2>Projects</h2>
+      <h3>Solar Cell Research Application</h3>
+      <h3>NASA Mission Control Project</h3>
+
+      <h2>Education</h2>
+      <h3>Master of Science in Mathematics</h3>
+      <h4>Texas A&M University-Central Texas</h4>
+      <h4>2020 - 2022</h4>
+      <h3>Bachelor of Science in Mathematics</h3>
+      <h4>Texas A&M University-Central Texas</h4>
+      <h4>2018 - 2020</h4>
+
+      <h2>Certifications</h2>
+      <h3>C Programming with Linux Specialization</h3>
+      <h4>September 2023</h4>
+      <p>Ad occaecat magna dolore nisi elit.</p>
+      <h3>Programming with JavaScript</h3>
+      <h4>August 2023</h4>
+      <p>Minim cillum culpa deserunt nisi anim occaecat occaecat adipisicing dolore elit nulla ex anim.</p>
+      <h3>Programming with Python</h3>
+      <h4>August 2022</h4>
+      <p>Voluptate officia deserunt proident id ut culpa aliqua sit mollit sit deserunt.</p>
     </main>
   )
 }
