fd119e633f0040a8a678fb0b7bb04d0af4e610a1
James <66404238+JSulley@users.noreply.github.com>
2023-10-24 08:16:36 -0500

    Added details for Projects, Education, and Certifications sections

--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,59 @@ export default function Home() {
   return (
     <main>
       <h1>James Sullivan</h1>
-      <h2>Aspiring Data Scientist/Software Engineer</h2>
+      <h3>Aspiring Data Scientist/Software Engineer</h3>
       <h3>About Me</h3>
       <p>Sint Lorem est dolor labore exercitation Lorem nisi laboris ex mollit.</p>
-
+      
       <h2>Projects</h2>
-      <h3>Solar Cell Research Application</h3>
       <h3>NASA Mission Control Project</h3>
+      <p>Project Description: Aliquip veniam commodo excepteur Lorem ullamco duis esse veniam nostrud elit ullamco.</p>
+      <p>Technologies Used</p>
+      <ul>
+        <li>Skill</li>
+      </ul>
+      <a>GitHub Repo</a>
+
+      <h3>Solar Cell Research Application</h3>
+      <p>Project Description: Culpa in officia eu est anim cillum.</p>
+      <p>Technologies Used</p>
+      <ul>
+        <li>Skill</li>
+      </ul>
+      <a>GitHub Repo</a>
 
       <h2>Education</h2>
-      <h3>Master of Science in Mathematics</h3>
-      <h4>Texas A&M University-Central Texas</h4>
-      <h4>2020 - 2022</h4>
-      <h3>Bachelor of Science in Mathematics</h3>
-      <h4>Texas A&M University-Central Texas</h4>
-      <h4>2018 - 2020</h4>
+      <h3>Degree: Master of Science in Mathematics</h3>
+      <h4>University: Texas A&M University-Central Texas</h4>
+      <h4>Time Frame: 2020 - 2022</h4>
+      <h3>Degree: Bachelor of Science in Mathematics</h3>
+      <h4>University: Texas A&M University-Central Texas</h4>
+      <h4>Time Frame: 2018 - 2020</h4>
 
       <h2>Certifications</h2>
-      <h3>C Programming with Linux Specialization</h3>
-      <h4>September 2023</h4>
-      <p>Ad occaecat magna dolore nisi elit.</p>
-      <h3>Programming with JavaScript</h3>
-      <h4>August 2023</h4>
-      <p>Minim cillum culpa deserunt nisi anim occaecat occaecat adipisicing dolore elit nulla ex anim.</p>
-      <h3>Programming with Python</h3>
-      <h4>August 2022</h4>
-      <p>Voluptate officia deserunt proident id ut culpa aliqua sit mollit sit deserunt.</p>
+      <h3>Name: C Programming with Linux Specialization</h3>
+      <p>Acquired Skills: Lorem</p>
+      <h4>Date Received: September 2023</h4>
+      <p>Description: Ad occaecat magna dolore nisi elit.</p>
+      <a>View Certificate</a>
+
+      <h3>Name: Introduction to Databases for Back-End Development</h3>
+      <p>Acquired Skills: Lorem</p>
+      <h4>Date Received: February 2023</h4>
+      <p>Description: Minim cillum culpa deserunt nisi anim occaecat occaecat adipisicing dolore elit nulla ex anim.</p>
+      <a>View Certificate</a>
+
+      <h3>Name: Programming with JavaScript</h3>
+      <p>Acquired Skills: Lorem</p>
+      <h4>Date Received: September 2022</h4>
+      <p>Description: Minim cillum culpa deserunt nisi anim occaecat occaecat adipisicing dolore elit nulla ex anim.</p>
+      <a>View Certificate</a>
+
+      <h3>Name: Programming with Python</h3>
+      <p>Acquired Skills: Lorem</p>
+      <h4>Date Received: August 2022</h4>
+      <p>Description: Voluptate officia deserunt proident id ut culpa aliqua sit mollit sit deserunt.</p>
+      <a>View Certificate</a>
     </main>
   )
 }
