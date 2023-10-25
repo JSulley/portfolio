09db6ac2918f34a04d1707f5ed0e913e233118bb
JS <66404238+JSulley@users.noreply.github.com>
2023-10-25 10:21:02 -0500

    Added semicolon

--- src/components/certificate-item/certificate-item.jsx
+++ src/components/certificate-item/certificate-item.jsx
@@ -1,12 +1,12 @@
 export default function CertificateItem({ certificate }) {
-    const { name, skillsLearned, dateReceived, description, hyperlink } = certificate
-    return (
-      <div>
-        <h3>{name}</h3>
-        <p>{skillsLearned}</p>
-        <h4>{dateReceived}</h4>
-        <p>{description}</p>
-        <a href={hyperlink} target="_blank">View Certificate</a>
-      </div>
-    );
+  const { name, skillsLearned, dateReceived, description, hyperlink } = certificate;
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{skillsLearned}</p>
+      <h4>{dateReceived}</h4>
+      <p>{description}</p>
+      <a href={hyperlink} target="_blank">View Certificate</a>
+    </div>
+  );
 };
