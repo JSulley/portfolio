2b9132801f4057fd1845a721c3d7c3cfdcaa5554
James <66404238+JSulley@users.noreply.github.com>
2023-11-25 03:43:07 -0600

    Added content and adjusted layout

--- src/components/certificate-item/certificate-item.jsx
+++ src/components/certificate-item/certificate-item.jsx
@@ -1,10 +1,10 @@
 export default function CertificateItem({ certificate }) {
   const { name, skillsLearned, dateReceived, description, hyperlink } = certificate;
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>{skillsLearned}</p>
-      <h4>{dateReceived}</h4>
+    <div className="even:mr-4 odd:ml-4 border-2 border-gray-950 rounded-xl mx-4pl-2 p-2">
+      <h3 className="text-xl mb-2">{name}</h3>
+      {skillsLearned.map(skill => <div key={skill.id} className="border-2 border-sky-400 bg-sky-400 rounded-xl mx-1 p-1 inline-block sm:max-lg:my-1">{skill.skill}</div>)}
+      <p className="mt-2">{dateReceived}</p>
       <p>{description}</p>
       <a href={hyperlink} target="_blank">View Certificate</a>
     </div>
--- src/components/certificates/certificates.jsx
+++ src/components/certificates/certificates.jsx
@@ -4,50 +4,76 @@ const certificates = [
   {
     'id': 1,
     'name': 'C Programming with Linux Specialization',
-    'skillsLearned': 'Lorem',
+    'skillsLearned': [
+      {
+        'id': 1,
+        'skill': 'Linux'
+      },
+      {
+        'id': 2,
+        'skill': 'C'
+      },
+    ],
     'dateReceived': 'September 2023',
-    'description': 'Ad occaecat magna dolore nisi elit.',
+    'description': 'Specialization by Dartmouth College and Institut Mines-Télécom covering the fundamentals of the C programming language, modular programming, memory management, and the command line interface in the Linux operating system.',
     'hyperlink': 'https://www.coursera.org/account/accomplishments/specialization/certificate/P87AL4S9V3HF',
   },
   {
     'id': 2,
-    'name': 'Django Web Framework',
-    'skillsLearned': 'Lorem',
-    'dateReceived': 'August 2023',
-    'description': 'Nulla enim ea excepteur cupidatat et deserunt voluptate reprehenderit id excepteur exercitation ea.',
-    'hyperlink': 'https://www.coursera.org/account/accomplishments/certificate/R5ZPX7HS6XHZ',
-  },
-  {
-    'id': 3,
     'name': 'Introduction to Databases for Back-End Development',
-    'skillsLearned': 'Lorem',
+    'skillsLearned': [
+      {
+        'id': 1,
+        'skill': 'SQL'
+      },
+      {
+        'id': 2,
+        'skill': 'Database Management Systems (DBMS)'
+      },
+      {
+        'id': 3,
+        'skill': 'SQLite'
+      }
+    ],
     'dateReceived': 'February 2023',
-    'description': 'Minim cillum culpa deserunt nisi anim occaecat occaecat adipisicing dolore elit nulla ex anim.',
+    'description': 'Course taught by Meta providing a comprehensive introduction to databases and their role in back-end development. It covered how to use Structured Query Language (SQL) to perform CRUD operations, operators, and sorting/filtering queries to subset data in databases. Finally, database design principles such as schema, relational database design, and normalization were introduced.',
     'hyperlink': 'https://www.coursera.org/account/accomplishments/certificate/54XGZYBWTT9C',
   },
   {
-    'id': 4,
+    'id': 3,
     'name': 'Programming with JavaScript',
-    'skillsLearned': 'Lorem',
+    'skillsLearned': [
+      {
+        'id': 1,
+        'skill': 'JavaScript'
+      },
+    ],
     'dateReceived': 'September 2022',
-    'description': 'Minim cillum culpa deserunt nisi anim occaecat occaecat adipisicing dolore elit nulla ex anim.',
+    'description': 'Course taught by Meta exploring the fundamentals of JavaScript (e.g., data types, operators, etc.), arrays, objects, functions, error prevention, and programming paradigms like functional programming and object-oriented programming. Additionally, it covered testing code using the Jest framework and how JavaScript operates in the browser.',
     'hyperlink': 'https://www.coursera.org/account/accomplishments/certificate/EHGYTLVDQRQH',
   },
   {
-    'id': 5,
+    'id': 4,
     'name': 'Programming in Python',
-    'skillsLearned': 'Lorem',
+    'skillsLearned': [
+      {
+        'id': 1,
+        'skill': 'Python'
+      }
+    ],
     'dateReceived': 'August 2022',
-    'description': 'Voluptate officia deserunt proident id ut culpa aliqua sit mollit sit deserunt.',
+    'description': 'Course taught by Meta that provided a comprehensive introduction to Python. It covered Python fundamentals (e.g., control flow, data types, functions), programming paradigms (e.g., procedural, and Object-Oriented Programming (OOP)), and the importance of testing using Test-Driven Development (TDD).',
     'hyperlink': 'https://www.coursera.org/account/accomplishments/certificate/G54R8FDXKV57',
   },
 ];
 
 export default function Certificates() {
   return (
-    <div>
-      <h2>Certificates</h2>
-      {certificates.map(certificate => <CertificateItem key={certificate.id} certificate={certificate} />)}
+    <div class="">
+      <h2 className="text-2xl mt-6 ml-2 mb-4">Certificates</h2>
+      <div className="grid gap-3 grid-cols-2">
+        {certificates.map(certificate => <CertificateItem key={certificate.id} certificate={certificate} />)}
+      </div>
     </div>
   );
 }
