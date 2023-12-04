0f432885f4f47e7266fe85469ddc1a9eb3881e6f
James <66404238+JSulley@users.noreply.github.com>
2023-12-03 22:48:25 -0600

    Added styling to "Education" section

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -5,21 +5,23 @@ const degrees = [
     'id': 1,
     'title': 'Master of Science in Mathematics',
     'university': 'Texas A&M University-Central Texas',
-    'timeFrame': '2020 - 2022'
+    'timeFrame': 'January 2020 - May 2022'
   },
   {
     'id': 2,
     'title': 'Bachelor of Science in Mathematics',
     'university': 'Texas A&M University-Central Texas',
-    'timeFrame': '2018 - 2020'
+    'timeFrame': 'August 2018 - May 2020'
   },
 ];
 
 export default function Education() {
   return (
     <div>
-      <h2>Education</h2>
-      {degrees.map(degree => <DegreeItem key={degree.id} degree={degree} />)}
+      <h2 className="text-2xl mt-6 ml-2 mb-4">Education</h2>
+      <div className="grid gap-3 grid-cols-2">
+        {degrees.map(degree => <DegreeItem key={degree.id} degree={degree} />)}
+      </div>
     </div>
   );
 }
