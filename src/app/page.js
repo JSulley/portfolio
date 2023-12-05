c31140a76aba8b4f56bd46607ce98e2ff7f16aec
James <66404238+JSulley@users.noreply.github.com>
2023-12-04 23:01:50 -0600

    Created component for introduction

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 import Projects from "@/components/projects/projects";
 import Education from "@/components/education/education";
 import Certificates from "@/components/certificates/certificates";
+import Introduction from "@/components/introduction/introduction";
 
 export default function Home() {
   return (
     <main>
-      <div className="text-center">
-        <h1 className="text-5xl my-2">James Sullivan</h1>
-        <h3>Aspiring Data Scientist/Software Engineer</h3>
-      </div>
-
+      <Introduction />
       <Projects />
       <Education />
       <Certificates />
