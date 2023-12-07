400c753405dcde7797032e3ca5c56e17d1f51d80
James <66404238+JSulley@users.noreply.github.com>
2023-12-06 23:20:43 -0600

    Restyled skills in "Certificates" and "Projects" sections

--- src/components/project-item/project-item.jsx
+++ src/components/project-item/project-item.jsx
@@ -4,7 +4,7 @@ export default function ProjectItem({ project }) {
   return (
     <div className="even:mr-4 odd:ml-4 border-2 border-gray-950 rounded-xl mx-4pl-2 p-2">
       <h3 className="text-xl mb-2">{title}</h3>
-      {techStack.map(skill => <div key={skill.id} className="border-2 border-sky-400 bg-sky-400 rounded-xl mx-1 p-1 inline-block sm:max-lg:my-1">{skill.skill}</div>)}
+      {techStack.map(skill => <div key={skill.id} className="border-2 border-[#9ecaff]/10 bg-[#9ecaff]/40 text-[#9ecaff] rounded-xl mx-1 p-1 inline-block sm:max-lg:my-1">{skill.skill}</div>)}
       <p>{description}</p>
       <a href={repoLink} target="_blank">View on GitHub</a>
     </div>
