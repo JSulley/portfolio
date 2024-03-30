12f4afbb8774126aaeaf8a23ec80fdbc171ea2c6
James <66404238+JSulley@users.noreply.github.com>
2024-03-30 02:40:56 -0500

    Added current degree program

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -3,12 +3,18 @@ import DegreeItem from '../degree-item/degree-item'
 const degrees = [
   {
     'id': 1,
+    'title': 'Bachelor of Science in Computer Science',
+    'university': 'University of Texas at San Antonio',
+    'timeFrame': 'January 2024 - May 2027'
+  },
+  {
+    'id': 2,
     'title': 'Master of Science in Mathematics',
     'university': 'Texas A&M University-Central Texas',
     'timeFrame': 'January 2020 - May 2022'
   },
   {
-    'id': 2,
+    'id': 3,
     'title': 'Bachelor of Science in Mathematics',
     'university': 'Texas A&M University-Central Texas',
     'timeFrame': 'August 2018 - May 2020'
