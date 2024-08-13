b5d9daebd892164a769edd14e82f270f55a1a2c0
James <66404238+JSulley@users.noreply.github.com>
2024-08-13 00:09:37 -0500

    Applied styles for section title "Projects" to "Education"

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -5,27 +5,27 @@ const degrees = [
     'id': 1,
     'title': 'Bachelor of Science in Computer Science',
     'university': 'The University of Texas at San Antonio',
-    'timeFrame': 'January 2024 - May 2027'
+    'timeFrame': 'May 2027'
   },
   {
     'id': 2,
     'title': 'Master of Science in Mathematics',
     'university': 'Texas A&M University-Central Texas',
-    'timeFrame': 'January 2020 - May 2022'
+    'timeFrame': 'May 2022'
   },
   {
     'id': 3,
     'title': 'Bachelor of Science in Mathematics',
     'university': 'Texas A&M University-Central Texas',
-    'timeFrame': 'August 2018 - May 2020'
+    'timeFrame': 'May 2020'
   },
 ];
 
 export default function Education() {
   return (
     <div>
-      <h2 className="text-2xl mt-6 ml-2 mb-4">Education</h2>
-      <div className="grid gap-3 grid-cols-2">
+      <h2 className="text-3xl mt-6 text-center font-bold">Education</h2>
+      <div className="flex flex-wrap flex-col space-y-4 m-4">
         {degrees.map(degree => <DegreeItem key={degree.id} degree={degree} />)}
       </div>
     </div>
