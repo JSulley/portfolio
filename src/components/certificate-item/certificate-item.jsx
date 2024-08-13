6abaad0d3500468166c195538dddcca4aafe6318
James <66404238+JSulley@users.noreply.github.com>
2024-08-13 02:38:09 -0500

    Adjusted styles to match project items

--- src/components/certificate-item/certificate-item.jsx
+++ src/components/certificate-item/certificate-item.jsx
@@ -1,25 +1,31 @@
-import Image from "next/image";
-import ExternalLinkIcon from "../../../public/external-link-icon.svg";
-
 export default function CertificateItem({ certificate }) {
-  const { name, skillsLearned, dateReceived, description, hyperlink } = certificate;
+  const { name, institution, skillsLearned, dateReceived, description, hyperlink } = certificate;
   
   return (
-    <div className="even:mr-4 odd:ml-4 border-2 border-gray-950 rounded-xl mx-4pl-2 p-2">
-      <div className="text-xl mb-2 align-middle">
-        <a href={hyperlink} target="_blank" className="hover:underline">
-          {name}
-          <Image
-            src={ExternalLinkIcon}
-            width={32}
-            height={32}
-            className="inline"
-          />
+    <div className="border-2 border-gray-950 rounded-xl mx-4pl-2 p-2">
+
+      {/* Course Title */}
+      <span className="text-xl font-semibold">{name}</span>
+
+      {/* Course Information */}
+      <p>{institution}</p>
+      <p>Completed: {dateReceived}</p>
+      <p className="break-all hyphens-auto mt-4 mb-4">{description}</p>
+
+      {/* Skills */}
+      {skillsLearned.map(skill => (
+        <div
+          key={skill.id}
+          className="border-2 border-[#9ecaff]/10 bg-[#9ecaff]/40 text-[#9ecaff] rounded-xl mx-1 p-1 inline-block sm:max-lg:my-1 mb-2"
+        >
+          {skill.skill}
+        </div>
+      ))}
+
+        <a href={hyperlink} className="block mt-2 hover:underline">
+          View Certificate
         </a>
-      </div>
-      {skillsLearned.map(skill => <div key={skill.id} className="border-2 border-[#9ecaff]/10 bg-[#9ecaff]/40 text-[#9ecaff] rounded-xl mx-1 p-1 inline-block sm:max-lg:my-1">{skill.skill}</div>)}
-      <p className="mt-2">{dateReceived}</p>
-      <p className="break-all hyphens-auto">{description}</p>
+
     </div>
   );
 };
