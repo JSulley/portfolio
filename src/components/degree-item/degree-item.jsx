cfa05648339d9d42f90cef0107eb403e0b54490d
James <66404238+JSulley@users.noreply.github.com>
2024-08-16 21:41:08 -0500

    Minor margin style changes to maintain consistency

--- src/components/degree-item/degree-item.jsx
+++ src/components/degree-item/degree-item.jsx
@@ -3,8 +3,8 @@ export default function Degree({ degree }) {
 
   return (
     <div className="border-2 border-gray-950 rounded-xl mx-4pl-2 p-2">
-      <h3 className="text-xl font-semibold mb-1">{title}</h3>
-      <p>{university}</p>
+      <h3 className="text-xl font-semibold">{title}</h3>
+      <p className="mt-1">{university}</p>
       <p>{timeFrame}</p>
     </div>
   );
