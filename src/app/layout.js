a647894b2625ce0d2e9aa19cf2c7808029c277b5
James <66404238+JSulley@users.noreply.github.com>
2025-04-18 01:05:56 -0500

    Created icons folder; Removed unused icon; Added several SVG icons; Renamed icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,24 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'James Sullivan',
-  description: 'A portfolio website',
+  title: "James Sullivan",
+  description: "A portfolio website",
   metadataBase: new URL("https://jamessullivan.dev/"),
   openGraph: {
-    images: '/opengraph-image.png',
-  }
-}
+    images: "/opg-image.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <body className={`${inter.className} bg-slate-800  text-[#87AED4]`} >{children}</body>
+      <body className={`${inter.className} bg-slate-800 text-[#87AED4]`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@ import Introduction from "@/components/introduction/introduction";
 import Projects from "@/components/projects/projects";
 import Education from "@/components/education/education";
 import Certificates from "@/components/certificates/certificates";
-import Footer from "@/components/footer"
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="mx-auto md:w-[900px]">
+    <div className="mx-auto md:w-[900px] lg:w-[1024px]">
       <main>
         <Introduction />
         <Projects />
