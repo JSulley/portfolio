97b5f8f819e6113c95ac7bc3be987dc5d0cb03aa
James <66404238+JSulley@users.noreply.github.com>
2026-01-13 22:03:00 -0600

    Refactor projects array by putting project info in single object; Update affected code

--- src/components/project-item/project-item.jsx
+++ src/components/project-item/project-item.jsx
@@ -3,10 +3,8 @@ import GitHub from "@/assets/icons/github.svg";
 import ExternalLink from "@/assets/icons/external-link.svg";
 
 function GitHubLink({ project }) {
-  const { repoLink } = project;
-
   return (
-    <a href={repoLink} target="_blank">
+    <a href={project.links.repoLink} target="_blank">
       <Image
         src={GitHub}
         width={32}
@@ -19,10 +17,8 @@ function GitHubLink({ project }) {
 }
 
 function DemoLink({ project }) {
-  const { demoLink } = project;
-
   return (
-    <a href={demoLink} target="_blank">
+    <a href={project.links.demoLink} target="_blank">
       <Image
         src={ExternalLink}
         width={32}
@@ -35,21 +31,21 @@ function DemoLink({ project }) {
 }
 
 export default function ProjectItem({ project }) {
-  const { title, description, techStack } = project;
-
   return (
     <div className="flex h-full flex-col rounded-xl border-2 border-gray-950 bg-gray-950 p-4">
       {/* Project Title */}
-      <span className="block text-xl font-semibold text-white">{title}</span>
+      <span className="block text-xl font-semibold text-white">
+        {project.title}
+      </span>
 
       {/* Description */}
       <p className="mb-3 mt-1 hidden md:line-clamp-2 lg:line-clamp-3">
-        {description}
+        {project.description}
       </p>
 
       {/* Tech Stack */}
       <div className="my-2">
-        {techStack.map((skill) => (
+        {project.techStack.map((skill) => (
           <div
             key={skill.id}
             className="m-1 inline-block rounded-xl border-2 border-[#9ecaff]/10 bg-[#9ecaff]/40 p-1 text-[#9ecaff]"
@@ -61,8 +57,8 @@ export default function ProjectItem({ project }) {
 
       {/* Project Links */}
       <div className="mt-auto text-center">
-        {"repoLink" in project && <GitHubLink project={project} />}
-        {"demoLink" in project && <DemoLink project={project} />}
+        {project.links.repoLink && <GitHubLink project={project} />}
+        {project.links.demoLink && <DemoLink project={project} />}
       </div>
     </div>
   );
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,68 +1,57 @@
 import ProjectItem from "../project-item/project-item";
 
-const projectTitles = [
-  "Custom CI/CD Pipeline",
-  "Perudo Game Implementation",
-  "Portfolio",
-  "Solar Cell Research Application",
-];
-
-const projectDescriptions = [
-  "Currently developing a shell-script-driven CI/CD pipeline to automate build and test workflows for my Perudo game and other personal C projects. Focused on local automation, with plans to integrate GitHub Actions for remote builds and deployment.",
-  "Developing a terminal-based version of the classic Perudo game using C. Features a turn-based bidding system between a human player and a computer opponent, with full implementation of core game rules. Designed with a focus on portability, memory safety, and clean code structure.",
-  "You're looking at it! What's infinite recursion you ask? Well, click on that external link icon below (the one next to the GitHub icon) and find out!",
-  "Shiny web app built for the Engineering department at Texas A&M University-Central Texas.",
-];
-
-const projectSkills = [
-  ["Bash", "Linux/Unix"],
-  ["C"],
-  ["JavaScript", "Next.js", "Tailwind CSS"],
-  ["R", "Shiny", "ggplot2"],
-];
-
-const projectLinks = [
+const projects = [
   {
-    repoLink: null,
-    demoLink: null,
+    title: "Custom CI/CD Pipeline",
+    description:
+      "Currently developing a shell-script-driven CI/CD pipeline to automate build and test workflows for my Perudo game and other personal C projects. Focused on local automation, with plans to integrate GitHub Actions for remote builds and deployment.",
+    tech: ["Bash", "Linux/Unix"],
+    links: {
+      repoLink: null,
+      demoLink: null,
+    },
   },
   {
-    repoLink: null,
-    demoLink: null,
+    title: "Perudo Game Implementation",
+    description:
+      "Developing a terminal-based version of the classic Perudo game using C. Features a turn-based bidding system between a human player and a computer opponent, with full implementation of core game rules. Designed with a focus on portability, memory safety, and clean code structure.",
+    tech: ["C"],
+    links: {
+      repoLink: null,
+      demoLink: null,
+    },
   },
   {
-    repoLink: "https://github.com/JSulley/portfolio",
-    demoLink: "https://jamessullivan.dev",
+    title: "Portfolio",
+    description:
+      "You're looking at it! What's infinite recursion you ask? Well, click on that external link icon below (the one next to the GitHub icon) and find out!",
+    tech: ["JavaScript", "Next.js", "Tailwind CSS"],
+    links: {
+      repoLink: "https://github.com/JSulley/portfolio",
+      demoLink: "https://jamessullivan.dev",
+    },
   },
   {
-    repoLink: "https://github.com/JSulley/Solar-Cell-Research-Application",
-    demoLink: "https://sulley.shinyapps.io/Solar_Cell_Research_App/",
+    title: "Solar Cell Research Application",
+    description:
+      "Shiny web app built for the Engineering department at Texas A&M University-Central Texas.",
+    tech: ["R", "Shiny", "ggplot2"],
+    links: {
+      repoLink: "https://github.com/JSulley/Solar-Cell-Research-Application",
+      demoLink: "https://sulley.shinyapps.io/Solar_Cell_Research_App/",
+    },
   },
 ];
 
-const projects = Array(projectTitles.length);
+for (let i = 0; i < projects.length; i++) {
+  projects[i].id = i + 1;
 
-for (let i = 0; i < projectTitles.length; i++) {
-  projects[i] = {
-    id: i + 1,
-    title: projectTitles[i],
-    description: projectDescriptions[i],
-    techStack: Array(projectSkills[i].length),
-  };
-
-  for (let j = 0; j < projectSkills[i].length; j++) {
-    projects[i].techStack[j] = {
+  projects[i].techStack = [];
+  for (let j = 0; j < projects[i].tech.length; j++) {
+    projects[i].techStack.push({
       id: j + 1,
-      skill: projectSkills[i][j],
-    };
-  }
-
-  if (projectLinks[i].repoLink) {
-    projects[i].repoLink = projectLinks[i].repoLink;
-  }
-
-  if (projectLinks[i].demoLink) {
-    projects[i].demoLink = projectLinks[i].demoLink;
+      skill: projects[i].tech[j],
+    });
   }
 }
 
