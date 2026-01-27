1097a4b6d5b8732e79b4fb142c0484b8bc2b860d
James <66404238+JSulley@users.noreply.github.com>
2026-01-26 23:58:14 -0600

    Update styling for Education section

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -25,7 +25,7 @@ export default function Education() {
   return (
     <div>
       <h2 className="my-6 text-center text-3xl font-bold">Education</h2>
-      <div className="mx-auto grid w-full max-w-sm max-w-xs gap-4 sm:max-w-xl md:max-w-2xl md:grid-cols-2 lg:max-w-4xl lg:grid-cols-3 xl:max-w-5xl 2xl:max-w-7xl">
+      <div className="mx-auto grid w-full gap-6 px-4 sm:max-w-xl md:max-w-2xl md:grid-cols-2 lg:max-w-4xl lg:grid-cols-3 xl:max-w-5xl 2xl:max-w-7xl">
         {degrees.map((degree) => (
           <DegreeItem key={degree.id} degree={degree} />
         ))}
